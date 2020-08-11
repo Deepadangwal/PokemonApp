6553883b0b9054216001970465545e8ca11c3fae
Deepa Dangwal <dangwaldeepa19@gmail.com>
2020-08-11 17:13:58 +0530

    Updates

---
+++ precache-manifest.1672a9568f3f08d14c6cfcde0408373d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8b7ffa730f469228c0e4e64b8569b6",
+    "url": "/pokemonApp/index.html"
+  },
+  {
+    "revision": "179619fba6e98039e94f",
+    "url": "/pokemonApp/static/css/main.b7aaeda8.chunk.css"
+  },
+  {
+    "revision": "3f4b671ec9eda10f0f36",
+    "url": "/pokemonApp/static/js/2.7d2ed113.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pokemonApp/static/js/2.7d2ed113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179619fba6e98039e94f",
+    "url": "/pokemonApp/static/js/main.cd9fd0f3.chunk.js"
+  },
+  {
+    "revision": "f50bac40d89ea0b4790a",
+    "url": "/pokemonApp/static/js/runtime-main.b2ab7484.js"
+  }
+]);
